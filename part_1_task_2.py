952a24e595dc8b6fb8256fdbb5254af27a6eb48b
Harith Rathish <harith1996@gmail.com>
2024-06-04 16:28:56 +0200

    updated task 3 and task 2

--- part_1_task_2.py
+++ part_1_task_2.py
@@ -1,33 +1,88 @@
-#find the most important features to predict rg_case
-
+# find the most important features to predict rg_case
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
 from sklearn import preprocessing
-import scipy.stats as stats
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+# categorical columns
+CAT_COLS = ["country_name", "language_name", "gender"]
+
+# date columns
+DATE_COLS = ["first_deposit_date", "registration_date"]
 
 
+def preprocess(df):
+    # transform categorical to numerical
+    le = preprocessing.LabelEncoder()
+    for col in CAT_COLS:
+        df[col] = le.fit_transform(df[col])
 
-df = pd.read_csv('Gambling_data.csv', sep=";")
+    # transform date to month
+    for col in DATE_COLS:
+        df[col] = pd.to_datetime(df[col])
+        df[col] = df[col].dt.month
 
-#selecting the columns
-sub_df = df[df.columns[8:].union(['rg_case'])]
-le = preprocessing.LabelEncoder()
-df['country_name'] = le.fit_transform(df['country_name'])
-df['language_name'] = le.fit_transform(df['language_name'])
-df['gender'] = le.fit_transform(df['gender'])
-feat_importance = []
-for c in df.columns[8:].union(['rg_case', 'country_name', 'language_name', 'gender']):
-    df.dropna(subset=[c, 'rg_case'], inplace=True)
-    corr = stats.pointbiserialr(df['rg_case'], df[c])
-    feat_importance.append({
-        'column': c,
-        'correlation': corr[0],
-        'p-value': corr[1]
-    })
-    #sort by correlation
-feat_importance = pd.DataFrame(sorted(feat_importance, key=lambda x: abs(x['correlation']), reverse=True))
-print(feat_importance)
-# #sort by absolute value
-# rg_case_corr = rg_case_corr.reindex(rg_case_corr.abs().sort_values(ascending=False).index)
-# print(rg_case_corr)
+    return df
+
+
+def get_feature_importance(model, X, y):
+    model.fit(X, y)
+    return model.feature_importances_
+
+
+def print_feature_importance(importances, columns):
+    for i, v in enumerate(importances):
+        column = columns[i]
+        print("Feature: %0d, Score: %.5f" % (i, v), column)
+
+if __name__ == "__main__":
+    
+    main_df = pd.read_csv("Gambling_data.csv", sep=";")
+
+    # remove target variable
+    df = main_df.drop(columns=["rg_case"])
+    df = preprocess(df)
+
+    # transform sub_df to numpy array
+    X = df.to_numpy()
+
+    # extract the target variable
+    y = main_df["rg_case"].to_numpy()
+
+    # define and fit the models
+    random_for = RandomForestClassifier()
+    xgb = XGBClassifier()
+
+    # get importances
+    random_for_imp = get_feature_importance(random_for, X, y)
+    xgb_imp = get_feature_importance(xgb, X, y)
+
+    # summarize feature importance
+    print_feature_importance(random_for_imp, df.columns)
+    print_feature_importance(xgb_imp, df.columns)
+
+    fig = make_subplots(rows=1, cols=3)
+
+    fig.add_trace(
+        go.Bar(
+            x=[df.columns[x] for x in range(len(random_for_imp))],
+            y=random_for_imp,
+            name="Random Forest",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.add_trace(
+        go.Bar(x=[df.columns[x] for x in range(len(xgb_imp))], y=xgb_imp, name="XGBoost"),
+        row=1,
+        col=2,
+    )
+
+    fig.update_layout(height=600, width=1500, title_text="Side By Side Feature Importance")
+    fig.show()
--- part_1_task_3.py
+++ part_1_task_3.py
@@ -3,13 +3,33 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import preprocessing
 import scipy.stats as stats
-
-
+from part_1_task_2 import preprocess, get_feature_importance, print_feature_importance
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 df = pd.read_csv('Gambling_data.csv', sep=";")
-
 GAME_TYPES = ['fixedodds', 'liveaction', 'casino']
 COLUMN_PREFIX = 'duration'
+
+def filter_by_game_type(game_type:str):
+    """Return players that play ONLY the specified game type
+
+    Args:
+        game_type (str): _description_
+    """
+    game_type_column = COLUMN_PREFIX + '_' + game_type
+    filtered_df = df.copy()
+    filtered_df = filtered_df[filtered_df[game_type_column].notnull()]
+    #remove columns of other game types
+    for g in GAME_TYPES:
+        if g != game_type:
+            filtered_df = filtered_df.dropna(subset=[COLUMN_PREFIX + '_' + g])
+            droppable_columns = list(filter(lambda x : g in x, df.columns))
+            filtered_df = filtered_df.drop(columns=droppable_columns)
+    return filtered_df
+
 rg_cases = {}
 control_cases = {}
 for game_type in GAME_TYPES:
@@ -27,5 +47,33 @@ for index, row in df.iterrows():
                 else:
                     control_cases[game_type] += 1
 
-print("responsible_gamblers: ", rg_cases)
-print("control", control_cases)
+print("rg_cases: ", rg_cases)
+print("non_rg_cases", control_cases)
+
+fig = make_subplots(rows=1, cols=3)
+
+for i, game_type in enumerate(GAME_TYPES):
+    # get feature importance
+    X = filter_by_game_type(game_type)
+    X = preprocess(X)
+    y = X['rg_case']
+    X = X.drop(columns=['rg_case'])
+    columns = X.columns
+    X = X.to_numpy()
+    y = y.to_numpy()
+    model = XGBClassifier()
+    feature_importance = get_feature_importance(model, X, y)
+    print_feature_importance(feature_importance, columns)
+
+    fig.add_trace(
+        go.Bar(
+            x=[columns[x] for x in range(len(feature_importance))],
+            y=feature_importance,
+            name=game_type,
+        ),
+        row=1,
+        col=i+1,
+    )
+
+fig.update_layout(height=600, width=1500, title_text="Feature importance (XGBoost) by game type")
+fig.show()
